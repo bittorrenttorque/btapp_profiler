6118d264e2cee9ba0aae3705caaf2967e8978f5b
Patrick Williams <patrick@bittorrent.com>
2012-09-13 02:01:25 -0700

    updating html layout

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,30 +1,26 @@
 jQuery(function() {
-	function notice(event) {
-		$('body').append('<div class="alert alert-success">' + event + '</div>');
+	function set(id, text) {
+		$('#' + id).text(text);
+	}
+	function append(id, text) {
+		$('#' + id).append('<div>' + text + '</div>');
 	}
 
 	window.btapp = new Btapp();
 	btapp.connect();
 
-	btapp.on('add:add', function(add) {
-		//add.torrent('http://featuredcontent.utorrent.com/torrents/CountingCrows-BitTorrent.torrent')
-		//add.torrent('magnet:?xt=urn:btih:2110C7B4FA045F62D33DD0E01DD6F5BC15902179&dn=CountingCrows-BitTorrent&tr=udp://tracker.openbittorrent.com:80/announce')
-		notice('ready to add torrents');
+	btapp.on('add', function(obj) {
+		if(typeof obj !== 'object') return;
+		append('events', obj.path[1] + ' ready');
 	});
 
-	btapp.on('add:settings', function(settings) {
-		notice('ready to play with settings');
+	var torrents = 0;
+	btapp.live('torrent *', function() {
+		set('torrents', ++torrents + ' torrents');
 	});
 
-	btapp.on('add:torrent', function(torrent_list) {
-		notice('we have ' + torrent_list.length + ' torrents');
-	});
-
-	btapp.on('add:dht', function(dht) {
-		notice('we have access to the dht');
-	});
-
-	btapp.live('torrent * file * properties name', function(name) {
-		notice('we have a file in a torrent: ' + name);
+	var files = 0;
+	btapp.live('torrent * file *', function() {
+		set('files', ++files + ' files');
 	});
 });
